420b65f8de1ae7c4df798348d5a097fd4ad40d56
Gustavo <gustavomaescd@gmail.com>
2020-08-05 18:00:56 -0300

    feat: ensure login call validation with correct password value

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -18,6 +18,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
+    password: '',
     mainError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório'
@@ -26,6 +27,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     validation.validate({ email: state.email })
   }, [state.email])
+
+  useEffect(() => {
+    validation.validate({ password: state.password })
+  }, [state.password])
 
   return (
     <Container>
