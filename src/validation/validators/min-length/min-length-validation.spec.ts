2acb4b4159a189c8ee80c9ddc00ca424c6c1751a
Gustavo <gustavomaescd@gmail.com>
2020-08-10 22:15:48 -0300

    test: ensure MinLenghtValidation return falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -4,7 +4,13 @@ import { MinLenghtValidation } from './min-length-validation'
 test('MinLenghtValidation', () => {
   describe('should return error if value lenght is less that prop value', () => {
     const sut = new MinLenghtValidation('field', 5)
-    const error = sut.validate('123 ')
+    const error = sut.validate('123')
     expect(error).toEqual(new InvalidFiedlError())
+  })
+
+  describe('should return falsy if value is valid', () => {
+    const sut = new MinLenghtValidation('field', 5)
+    const error = sut.validate('123456')
+    expect(error).toBeFalsy()
   })
 })
