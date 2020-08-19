92cc9449a5863250b1777299dbbc10913b5464b7
Gustavo <gustavomaescd@gmail.com>
2020-08-18 22:34:30 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Context from '@/presentation/contexts/form/form-context'
 import Input from './input'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/presentation/styles/theme'
 
+const makeSut = (): RenderResult => {
+  return render(
+    <ThemeProvider theme={theme}>
+      <Context.Provider value={{ state: {} }}>
+        <Input name="field"/>
+      </Context.Provider>
+    </ThemeProvider>
+  )
+}
 describe('Input Component', () => {
   test('should begin with readonly', () => {
-    const { getByTestId } = render(
-      <ThemeProvider theme={theme}>
-        <Context.Provider value={{ state: {} }}>
-          <Input name="field"/>
-        </Context.Provider>
-      </ThemeProvider>
-    )
+    const { getByTestId } = makeSut()
     const input = getByTestId('field') as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
