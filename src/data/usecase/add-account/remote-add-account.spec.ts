e89eb25c2b14911da19727fa6d42d71125754cfe
Gustavo <gustavomaescd@gmail.com>
2020-08-23 21:28:04 -0300

    test: ensure RemoteAddAccount throws EmailInUseError if HttpPostClienty return 403

--- src/data/usecase/add-account/remote-add-account.spec.ts
+++ src/data/usecase/add-account/remote-add-account.spec.ts
@@ -1,8 +1,10 @@
 import { RemoteAddAccount } from './remote-add-account'
 import { HttpPostClientSpy } from '@/data/test'
-import { AddAccountParams } from '@/domain/usecases'
+import { HttpStatusCode } from '@/data/protocols/http'
 import { mockAddAccountParams } from '@/domain/test'
+import { AddAccountParams } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
+import { EmailInUseError } from '@/domain/errors'
 import faker from 'faker'
 
 type SutTypes = {
@@ -33,5 +35,12 @@ describe('RemoteAuthentication', () => {
     const addAccountParams = mockAddAccountParams()
     await sut.add(addAccountParams)
     expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
+
+  test('should throw EmailInUseError if HttpPostClient returns 403 ', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = { statusCode: HttpStatusCode.forbidden }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new EmailInUseError())
   })
 })
--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,2 +1,3 @@
 export * from './invalid-credations-error'
 export * from './unexpected-error'
+export * from './email-in-use-error'
