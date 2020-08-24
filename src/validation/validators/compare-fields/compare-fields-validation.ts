e56a304795aa926a01b1da9f4f536f0cbc488fc0
Gustavo <gustavomaescd@gmail.com>
2020-08-24 19:31:25 -0300

    test: ensure CompareFieldsValidation return falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -8,6 +8,6 @@ export class CompareFieldsValidation implements FieldValidation {
   ) { }
 
   validate (value: string): Error {
-    return new InvalidFiedlError()
+    return value !== this.valueToCompare ? new InvalidFiedlError() : null
   }
 }
